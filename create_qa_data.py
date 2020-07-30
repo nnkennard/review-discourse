dec4ea2c7c5bfa98308bca6c4e4bad8dc7d48e91
Neha Nayak <nnayak@umass.edu>
2020-07-30 18:00:32 -0400

    all this still doesnt work

--- create_qa_data.py
+++ create_qa_data.py
@@ -18,21 +18,8 @@ QACandidate = collections.namedtuple('QACandidate',
   'parent_chunk_idx child_chunk_idx maybe_question_text')
 
 
-def chunk(text):
+def chunk_text(text):
   return text.split("\n\n")
-
-def get_text(node_id, all_notes):
-  k = [note for note in all_notes if note.id == node_id]
-  assert len(k) == 1
-  return orl.NoteNode(k[0]).text
-
-def tokenize_chunks(chunks, tokenize_client):
-  tokenized_chunks = []
-  for i, chunk in enumerate(chunks):
-    annotated_sentences = tokenize_client.annotate(chunk)
-    tokenized_chunks.append([fields.split("\t")[TOKEN] for fields in
-      annotated_sentences.split("\n") if fields])
-  return tokenized_chunks
 
 class CommentPair(object):
   def __init__(self, parent_node, child_node, node_map, tokenize_client):
@@ -72,29 +59,7 @@ class CommentPair(object):
 
   def _get_qa_chunks(self, client):
     qas = []
-    for j, parent_chunk in enumerate(self.parent_chunks):
-      questions_by_punct = get_questions(parent_chunk, client)
-      for question in questions_by_punct:
-        qas.append(QACandidate(j, None, question))
-      for i, child_chunk in enumerate(self.child_chunks):
-        prefix = longest_starting_prefix(parent_chunk, child_chunk)
-        if parent_chunk and (nltk.edit_distance(parent_chunk, child_chunk) < 5
-            and i < len(self.child_chunks) - 1):
-          qas.append(QACandidate(j, i, None))
-        elif len(prefix) > 10 or prefixExistsAndNotAlpha(prefix):
-          qas.append(QACandidate(j, i+1, None))
-    return [qa._asdict() for qa in qas]
-            
-def get_questions(text, client):
-  conll_text = client.annotate(text)
-  listified = conll_lib.listify_non_doc_lines(conll_text.split("\n"))
-  questions = []
-  for sentence in listified:
-    if sentence[-1][TOKEN] == "?":
-      questions.append(" ".join(fields[TOKEN] for fields in sentence))
-
-  return questions
-
+    
 def get_nodes_from_map(child, parent, node_map):
   if parent is None:
     return None
@@ -106,10 +71,12 @@ def get_nodes_from_map(child, parent, node_map):
     return None
 
 def longest_starting_prefix(chunk1, chunk2):
-  for i, c1 in enumerate(chunk1):
-    if i == len(chunk2) or not chunk2[i] == c1:
-      return chunk1[:i]
-  return chunk1
+  text1 = " ".join(chunk1)
+  text2 = " ".join(chunk2)
+  for i, c1 in enumerate(text1):
+    if i == len(text2) or not text2[i] == c1:
+      return text1[:i]
+  return text1
 
 
 def prefixExistsAndNotAlpha(prefix):
@@ -131,63 +98,100 @@ class SuperNode(object):
     self.top_node_id = top_node.note_id
     self.node_id = self.top_node_id + "_super"
     self.included_comment_ids = [self.top_node_id]
+    self.processed = False
 
   def add_node(self, new_node):
     self.text += "\n" + new_node.text
     self.included_comment_ids.append(new_node.note_id)
 
+  def process(self, tokenize_client):
+    chunks = chunk_text(self.text)
+    self.tokenized_chunks = []
+    self.question_map = collections.defaultdict(list)
+    for i, chunk in enumerate(chunks):
+      tokenized_chunk = []
+      annotated_sentences = tokenize_client.annotate(chunk)
+      listified_sentences = conll_lib.listify_non_doc_lines(
+          annotated_sentences.split("\n"))
+      for sentence in listified_sentences:
+        if sentence[-1][TOKEN] == "?":
+          self.question_map[i].append(" ".join(fields[TOKEN] for fields in sentence))
+        tokenized_chunk += [fields[TOKEN] for fields in sentence]
+      self.tokenized_chunks.append(tokenized_chunk) 
+    self.processed = True
+
+def mini_edit_distance(chunk_tokens_1, chunk_tokens_2):
+  return nltk.edit_distance(" ".join(chunk_tokens_1)[:100], 
+      " ".join(chunk_tokens_2)[:100])
+
+def find_chunk_pairs(parent_snode, child_snode):
+  print("Finding chunk pairs", parent_snode.node_id, child_snode.node_id)
+  qas = []
+  for i, parent_chunk in enumerate(parent_snode.tokenized_chunks):
+    for question in parent_snode.question_map[i]:
+      qas.append(QACandidate(i, None, question))
+    for j, child_chunk in enumerate(child_snode.tokenized_chunks):
+      if parent_chunk == child_chunk:
+        dsds
+      prefix = longest_starting_prefix(parent_chunk, child_chunk)
+      if (parent_chunk
+          and mini_edit_distance(parent_chunk, child_chunk) < 5
+          and j < len(child_snode.tokenized_chunks) - 1):
+        qas.append(QACandidate(i, j, None))
+      elif len(prefix) > 10 or prefixExistsAndNotAlpha(prefix):
+        qas.append(QACandidate(i, j+1, None))
+  return [qa._asdict() for qa in qas]
+
 
 def condense_long_comments(forum_structure, mini_node_map):
-  print(len(forum_structure))
-  print(forum_structure)
   maybe_roots = [key for key, val in forum_structure.items() if val is None]
   assert len(maybe_roots) == 1
   root_id, = maybe_roots
 
-  super_nodes = set()
-  super_node_map = {} # Map from node ids to the ids of their super nodes
+  stack = [root_id] # ids of comments to check
 
-  stack = [root_id]
-  follow_on_map = {} # from children to the supernode they are children of
+  super_node_parent = {}
+  super_node_map = {} # Should be a map from a super node to its supe rnode parent
+  follow_on_map = {}
+
   while stack:
-    print(len(stack), stack)
-    if len(stack) > 10:
-      break
     curr_node_id = stack.pop(0)
     curr_node = mini_node_map[curr_node_id]
 
-    super_node_map[curr_node_id] = super_node.node_id
-    super_nodes.add(super_node)
-
-    if curr_node_id in super_node_map:
-      super_node = super_node_map[curr_node_id]
-    else:
+    super_node = super_node_map.get(curr_node_id, None)
+    if super_node is None:
       super_node = SuperNode(curr_node)
+      super_node_map[curr_node_id] = super_node
+    child_ids = [child
+        for child, parent in forum_structure.items()
+        if parent == curr_node_id]
+    for child_id in child_ids:
+      stack.append(child_id)
+      child_node = mini_node_map[child_id]
+      if child_node.author == curr_node.author:
+        # This is a follow up
+        super_node.add_node(child_node)
+        follow_on_map[child_id] = super_node.node_id
 
 
-    for child, parent in forum_structure.items():
-      if parent == curr_node_id: # Only for direct children of current node
-        if child in follow_on_map: # This child is actually a follow on that has already been included
-          # Check that the child's super node is also the parent's super node
-          assert super_node_map[child] == super_node_map[parent]
-          continue
-        stack.append(child)
-        child_node = mini_node_map[child]
-        if child_node.author == curr_node.author:
-          super_node.add_node(child_node)
-          follow_on_map[child] = super_node.top_node_id
-      else:
-        continue
+  print("Printing super node map")
+  for k,v in super_node_map.items():
+    print(k, v)
 
-  if follow_on_map:
-    print(follow_on_map)
-    print(forum_structure)
-    print("follow on map", len(follow_on_map))
-    print(len(forum_structure))
-    print(len(super_nodes))
-    exit()
+  super_node_structure = {child.node_id:parent.node_id for child, parent in
+      super_node_map.items()}
+  print("Printing super node structire")
+  for k,v in super_node_structure.items():
+    print(k, v)
 
-    
+  all_super_nodes = set(
+      super_node_map.keys()).union(set(super_node_map.values())) 
+  super_node_id_map = {}
+  for super_node in all_super_nodes:
+    super_node_id_map[super_node.node_id] = super_node
+
+  return super_node_structure, super_node_id_map, follow_on_map
+
 
 def main():
 
@@ -200,23 +204,23 @@ def main():
 
       for forum, structure in tqdm(dataset.forum_map.items()):
         mini_node_map = get_mini_node_map(structure, dataset.node_map)
-        condense_long_comments(structure, mini_node_map)
-        continue
+        super_node_structure, super_node_id_map, follow_on_map = condense_long_comments(structure, mini_node_map)
+        for super_node in super_node_id_map.values():
+          super_node.process(corenlp_client)
 
-        for child, parent in structure.items():
-          maybe_nodes = get_nodes_from_map(child, parent, mini_node_map)
+        for child, parent in super_node_structure.items():
+          print("*", child, parent)
+          maybe_nodes = get_nodes_from_map(child, parent, super_node_id_map)
           if maybe_nodes is None:
             continue
           child, parent = maybe_nodes
-          new_qa_pair = CommentPair(
-              parent, child, mini_node_map, corenlp_client)
-          qa_pairs.append(new_qa_pair.example._asdict())
+          qa_pairs.append(find_chunk_pairs(parent, child))
 
       output_obj = {
-          "conference": dataset.conference,
-          "split": split,
-          "qa_pairs": qa_pairs
-          }
+      "conference": dataset.conference,
+      "split": split,
+      "qa_pairs": qa_pairs
+      }
 
       output_filename = "_".join(
           [output_prefix + "qaPairs", dataset.conference, dataset.split + ".json"])
