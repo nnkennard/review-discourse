e803b4bd068299a28d22d110ff18196f7c0cabc4
Neha Nayak <nnayak@umass.edu>
2020-08-09 20:17:32 -0400

    this sohuld create data in the new format

--- create_qa_data.py
+++ create_qa_data.py
@@ -10,26 +10,12 @@ from tqdm import tqdm
 
 import openreview_lib as orl
 
-
-QAExample = collections.namedtuple('QAExample',
-  'parent_id parent_chunks child_id child_chunks qas')
-
-QACandidate = collections.namedtuple('QACandidate',
-  'parent_id parent_chunk_idx child_id child_chunk_idx maybe_question_text')
-
+SupNode = collections.namedtuple('SupNode', 'supnode_id tokens')
+Question = collections.namedtuple('Question', 'supnode_id start exclusive_end')
+Answer = collections.namedtuple('Answer', 'supnode_id question start exclusive_end')
 
 def chunk_text(text):
   return text.split("\n\n")
-   
-def get_nodes_from_map(child, parent, node_map):
-  if parent is None:
-    return None
-  child_node = node_map[child]
-  parent_node = node_map[parent]
-  if child_node.text and parent_node.text:
-    return child_node, parent_node
-  else:
-    return None
 
 
 def longest_starting_prefix(chunk1, chunk2):
@@ -62,21 +48,7 @@ def mini_edit_distance(chunk_tokens_1, chunk_tokens_2):
       squish(chunk_tokens_2)[:100])
 
 
-def find_chunk_pairs(parent_snode, child_snode):
-  parent_id, child_id = parent_snode.node_id, child_snode.node_id
-  qas = []
-  for i, parent_chunk in enumerate(parent_snode.tokenized_chunks):
-    for question in parent_snode.question_map[i]:
-      qas.append(QACandidate(parent_id, i, child_id, None, question))
-    for j, child_chunk in enumerate(child_snode.tokenized_chunks):
-      prefix = longest_starting_prefix(parent_chunk, child_chunk)
-      if (parent_chunk
-          and mini_edit_distance(parent_chunk, child_chunk) < 5
-          and j < len(child_snode.tokenized_chunks) - 1):
-        qas.append(QACandidate(parent_id, i, child_id, j, None))
-      elif len(prefix) > 10 or prefixExistsAndNotAlpha(prefix):
-        qas.append(QACandidate(parent_id, i, child_id, j+1, None))
-  return [qa._asdict() for qa in qas]
+SEPARATOR_CHUNK = [["CHUNK_SEP"]]
 
 
 class SuperNode(object):
@@ -90,13 +62,14 @@ class SuperNode(object):
     self.included_nodes = [node_id]
     self.question_map = None
     self.tokenized_chunks = None
+    self.tokenized = False
 
 
   def serialize(self):
     return {
         "node_id": self.node_id,
         "included_nodes": self.included_nodes,
-        "text": [sum(chunk, []) for chunk in self.tokenized_chunks]
+        "tokens": sum([sum(chunk, []) for chunk in self.tokenized_chunks], [])
         }
 
   def add_child(self, child):
@@ -107,7 +80,9 @@ class SuperNode(object):
     self.children += node.children
     self.included_nodes.append(node.original_id)
 
-  def process(self, tokenize_client):
+  def tokenize(self, tokenize_client):
+    if self.tokenized:
+      return
     chunks = chunk_text(self.text)
     self.tokenized_chunks = []
     for chunk in chunks:
@@ -118,14 +93,25 @@ class SuperNode(object):
         sentence_tokens = [fields[TOKEN] for fields in sentence if fields]
         sentences.append(sentence_tokens)
       if sentences:
+        if self.tokenized_chunks:
+          self.tokenized_chunks.append(SEPARATOR_CHUNK)
         self.tokenized_chunks.append(sentences)
-
+    self.tokenized = True
+  
   def get_questions(self):
-    self.question_map = collections.defaultdict(list)
-    for i, chunk in enumerate(self.tokenized_chunks):
+    assert self.tokenized
+    questions = []
+
+    token_offset = 0
+    for chunk in self.tokenized_chunks:
       for sentence in chunk:
         if sentence[-1] == "?":
-          self.question_map[i].append(" ".join(sentence))
+          questions.append(
+              Question(self.node_id,
+                token_offset, token_offset + len(sentence)))
+        token_offset += len(sentence)
+    
+    return questions
 
   def __str__(self):
     return self.node_id
@@ -133,10 +119,12 @@ class SuperNode(object):
 def superify(old_id):
   return old_id + "_super"
 
-def restructure(forum_structure, node_map, tokenize_client):
+def restructure(forum_structure, node_map):
+  """Collapse continuations over multiple nodes."""
   supernode_map = {superify(key):SuperNode(
     value.note_id, value.title, value.text, value.author) for key, value in node_map.items()}
 
+  # Same structure, with superified names
   supernode_structure = {}
   for child, parent in forum_structure.items():
     if parent is None:
@@ -144,6 +132,7 @@ def restructure(forum_structure, node_map, tokenize_client):
     else:
       supernode_structure[superify(child)] = superify(parent)
 
+  # Absorb children with the same author into parent node
   for child, parent in supernode_structure.items():
     if parent is None:
       continue
@@ -156,39 +145,89 @@ def restructure(forum_structure, node_map, tokenize_client):
 
   new_supernode_structure = {}
   root, = [child
-      for child, parent in supernode_structure.items() if parent is None]
+            for child, parent in supernode_structure.items()
+            if parent is None]  # Should only be one such node
   stack = [(root, None)]
-  while stack:
+  while stack:  # Build new supernode structure
     curr_node, parent = stack.pop(0)
     new_children = supernode_map[curr_node].children
     stack += [(child, curr_node) for child in new_children]
     new_supernode_structure[curr_node] = parent
 
+  # Build map of supernodes that occur in the final structure
   final_nodes = set(new_supernode_structure.keys()).union(
       set(new_supernode_structure.values())) - set([None])
-  for node in final_nodes:
-    supernode_map[node].process(tokenize_client)
-
-  original_nodes = set(supernode_structure.keys()).union(
-      set(supernode_structure.values())) - set([None])
-
-  for parent_node_id in set(new_supernode_structure.values()):
-    # If it's not a parent, questions cannot have been answered
-    if parent_node_id is None:
-      continue
-    supernode_map[parent_node_id].get_questions()
 
   supernode_map = {key: value for key, value in supernode_map.items() if key in
       final_nodes}
 
-  qas = []
-  for child, parent in new_supernode_structure.items():
+  return new_supernode_structure, supernode_map
+
+
+def get_chunks(node):
+  chunks = []
+  assert node.tokenized
+  offset = 0
+  for chunk in node.tokenized_chunks:
+    updated_offset = offset + len(sum(chunk, []))
+    if not chunk == SEPARATOR_CHUNK:
+      chunks.append(((offset, updated_offset), chunk))
+    offset = updated_offset
+  return chunks
+
+
+def find_chunk_pairs(child_node, parent_node):
+  parent_chunks = get_chunks(parent_node)
+  child_chunks = get_chunks(child_node)
+
+  questions = []
+  answers = []
+  
+  for i, (parent_offsets, parent_chunk) in enumerate(parent_chunks):
+    for j, (child_offsets, child_chunk) in enumerate(child_chunks):
+      prefix = longest_starting_prefix(parent_chunk, child_chunk)
+      if (parent_chunk
+          and mini_edit_distance(parent_chunk, child_chunk) < 5
+          and j < len(child_chunks) - 1):
+        new_question = Question(parent_node.node_id, *parent_offsets)
+        questions.append(new_question)
+        answer = Answer(child_node.node_id, new_question, *child_offsets)
+        answers.append(answer)
+      elif len(prefix) > 10 or prefixExistsAndNotAlpha(prefix):
+        if j >= len(child_chunks) - 1:
+          continue
+        next_offsets, next_child_chunk = child_chunks[j+1]
+        new_question = Question(parent_node.node_id, *parent_offsets)
+        questions.append(new_question)
+        answer = Answer(child_node.node_id, new_question._asdict(), *next_offsets)
+        answers.append(answer)
+
+  return questions, answers
+
+
+def get_questions_and_answers(forum_structure, node_map, tokenize_client):
+
+
+  final_nodes = set(
+      forum_structure.keys()).union(
+          set(forum_structure.values())) - set([None])
+
+  questions = []
+  for node_id in final_nodes:
+    node = node_map[node_id]
+    node.tokenize(tokenize_client)
+    questions += node.get_questions()
+
+  answers = []
+  for child, parent in forum_structure.items():
     if parent is None:
       continue
-    qas += find_chunk_pairs(supernode_map[parent], supernode_map[child])
-
-  return supernode_map, qas
-
+    else:
+      new_qs, new_as = find_chunk_pairs(node_map[child], node_map[parent])
+      questions += new_qs
+      answers += new_as
+    
+  return questions, answers
 
 
 def main():
@@ -196,23 +235,28 @@ def main():
   dataset_file, output_prefix = sys.argv[1:]
 
   with corenlp.CoreNLPClient(annotators=ANNOTATORS, output_format='conll') as corenlp_client:
-    for split, dataset in orl.get_datasets(dataset_file).items():
+    for split, dataset in orl.get_datasets(dataset_file, debug=False).items():
 
       all_nodes = []
-      all_qas = []
+      all_questions = []
+      all_answers = []
 
       for forum, structure in tqdm(dataset.forum_map.items()):
         mini_node_map = get_mini_node_map(structure, dataset.node_map)
-        nodes, qas = restructure(structure, mini_node_map,
-            corenlp_client)
-        all_nodes += nodes.values()
-        all_qas += qas
+        supernode_structure, supernode_map = restructure(structure,
+            mini_node_map)
+        questions, answers = get_questions_and_answers(
+            supernode_structure, supernode_map, corenlp_client)
+        all_nodes += supernode_map.values()
+        all_questions += questions
+        all_answers += answers
     
       output_obj = {
       "conference": dataset.conference,
       "split": split,
       "nodes": [node.serialize() for node in all_nodes],
-      "qa_pairs": all_qas
+      "questions": [q._asdict() for q in all_questions],
+      "answers": [a._asdict() for a in all_answers]
       }
 
       output_filename = "_".join(
